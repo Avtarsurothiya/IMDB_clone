cec6dd278d6bd65e865a309540d6104a885a828d
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-25 15:53:47 +0530

    Implmenting Render list function for movie stack

--- js/script.js
+++ js/script.js
@@ -2,3 +2,46 @@ const homeButton = document.querySelector("#homeButton");
 const searchBox = document.querySelector(".form-control");
 const goToFavouriteButton = document.querySelector("#goto-favourites-button");
 const movieCardContainer = document.querySelector(".box")
+
+
+function showAlert(message) {
+    alert(message);
+  }
+  
+  let currentMovieStack = [];
+  
+  function renderList(actionForButton) {
+    movieCardContainer.innerHTML = '';
+    for (let i = 0; i < currentMovieStack.length; i++) {
+
+
+        let movieCard = document.createElement('div');
+        movieCard.classList.add("movie-card");
+    
+    
+        movieCard.innerHTML = `
+    <div class="card m-2" >
+            <img src="${'https://image.tmdb.org/t/p/w500' + currentMovieStack[i].poster_path}" alt="${currentMovieStack[i].title}" height="200" >
+                <div class="card-body">
+                    <h5 class="card-title"><span>${currentMovieStack[i].title}</span></h5>
+                    <div class="rating-container">
+                        <img src="./res/rating-icon.png" alt="">
+                        <span>${currentMovieStack[i].vote_average}</span>
+                    </div>
+                    <button id="${currentMovieStack[i].id}" onclick="getMovieInDetail(this)" class="btn btn-primary"> Movie Details </button>
+    
+                    <button onclick="${actionForButton}(this)" class="add-to-favourite-button text-icon-button btn btn-danger" data-id="${currentMovieStack[i].id}" >
+    
+                        <span>${actionForButton}</span>
+                    </button>
+                </div>
+    
+    
+    </div>
+            `;
+        movieCardContainer.append(movieCard); //appending card to the movie container view
+    
+      }
+    }
+    
+  
