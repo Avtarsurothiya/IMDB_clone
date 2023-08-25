fd80eb310c239d6c4d8efb16212d600e1dc9743c
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-25 15:55:32 +0530

    Adding favourite movie function

--- js/script.js
+++ js/script.js
@@ -44,4 +44,68 @@ function showAlert(message) {
       }
     }
     
+    
+
+// gets trending movies
+function getTrandingMovies() {
+    const tmdb = fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=cb213741fa9662c69add38c5a59c0110")
+      .then((response) => response.json())
+      .then((data) => {
+        currentMovieStack = data.results;
+        renderList("favourite");
+      })
+      .catch((err) => printError(err));
+  }
+  
+  homeButton.addEventListener('click', getTrandingMovies);
+  
+  getTrandingMovies();
+  addincurfav();
+  
+
+  // search box event listner check for any key press and search the movie according and show on web-page
+searchBox.addEventListener('keyup', () => {
+    let searchString = searchBox.value;
+  
+    if (searchString.length > 0) {
+      let searchStringURI = encodeURI(searchString);
+      const searchResult = fetch(`https://api.themoviedb.org/3/search/movie?api_key=cb213741fa9662c69add38c5a59c0110&language=en-US&page=1&include_adult=false&query=${searchStringURI}`)
+        .then((response) => response.json())
+        .then((data) => {
+          currentMovieStack = data.results;
+          renderList("favourite");
+        })
+        .catch((err) => printError(err));
+    }
+  })
+  
+
+// function to add movie into favourite section
+function favourite(element) {
+    let id = element.dataset.id;
+    for (let i = 0; i < currentMovieStack.length; i++) {
+      if (currentMovieStack[i].id == id) {
+        let favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
+  
+        if (favouriteMovies == null) {
+          favouriteMovies = [];
+        }
+  
+        for (let j = 0; j < favouriteMovies.length; j++) {
+          if (favouriteMovies[j].id == id) {
+            showAlert(currentMovieStack[i].title + " Already added to favourite")
+            return;
+          }
+        }
+  
+        favouriteMovies.unshift(currentMovieStack[i]);
+        localStorage.setItem("favouriteMovies", JSON.stringify(favouriteMovies));
+  
+        addincurfav();
+        showAlert(currentMovieStack[i].title + " added to favourite")
+        return;
+      }
+    }
+  }
+    
   
