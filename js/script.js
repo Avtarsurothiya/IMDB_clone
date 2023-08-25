f953a3f927962f44eab6acc39562dee53a9e8ab2
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-25 15:51:05 +0530

    Adding JS file for implementing functionality

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const homeButton = document.querySelector("#homeButton");
+const searchBox = document.querySelector(".form-control");
+const goToFavouriteButton = document.querySelector("#goto-favourites-button");
+const movieCardContainer = document.querySelector(".box")
