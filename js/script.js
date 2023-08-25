e3b0e27a266b2a6c6ae61b5c215f4b7804a1e423
Avtarsurothiya <avtarsurothiya@gmail.com>
2023-08-25 17:37:48 +0530

    Finishing up script.js and Project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Variable Definitions */
+
 const homeButton = document.querySelector("#homeButton");
 const searchBox = document.querySelector(".form-control");
 const goToFavouriteButton = document.querySelector("#goto-favourites-button");
@@ -6,19 +8,21 @@ const movieCardContainer = document.querySelector(".box")
 
 function showAlert(message) {
     alert(message);
-  }
-  
-  let currentMovieStack = [];
-  
-  function renderList(actionForButton) {
+}
+
+let currentMovieStack = [];
+
+/*   Function To render movie list */
+
+function renderList(actionForButton) {
     movieCardContainer.innerHTML = '';
     for (let i = 0; i < currentMovieStack.length; i++) {
 
 
         let movieCard = document.createElement('div');
         movieCard.classList.add("movie-card");
-    
-    
+
+
         movieCard.innerHTML = `
     <div class="card m-2" >
             <img src="${'https://image.tmdb.org/t/p/w500' + currentMovieStack[i].poster_path}" alt="${currentMovieStack[i].title}" height="200" >
@@ -40,84 +44,84 @@ function showAlert(message) {
     </div>
             `;
         movieCardContainer.append(movieCard); //appending card to the movie container view
-    
-      }
-    }
-    
-    
 
-// gets trending movies
+    }
+}
+
+
+
+// Function to fetch trending movies
 function getTrandingMovies() {
     const tmdb = fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=cb213741fa9662c69add38c5a59c0110")
-      .then((response) => response.json())
-      .then((data) => {
-        currentMovieStack = data.results;
-        renderList("favourite");
-      })
-      .catch((err) => printError(err));
-  }
-  
-  homeButton.addEventListener('click', getTrandingMovies);
-  
-  getTrandingMovies();
-  addincurfav();
-  
-
-  // search box event listner check for any key press and search the movie according and show on web-page
-searchBox.addEventListener('keyup', () => {
-    let searchString = searchBox.value;
-  
-    if (searchString.length > 0) {
-      let searchStringURI = encodeURI(searchString);
-      const searchResult = fetch(`https://api.themoviedb.org/3/search/movie?api_key=cb213741fa9662c69add38c5a59c0110&language=en-US&page=1&include_adult=false&query=${searchStringURI}`)
         .then((response) => response.json())
         .then((data) => {
-          currentMovieStack = data.results;
-          renderList("favourite");
+            currentMovieStack = data.results;
+            renderList("favourite");
         })
         .catch((err) => printError(err));
-    }
-  })
-  
+}
 
-// function to add movie into favourite section
+homeButton.addEventListener('click', getTrandingMovies);
+
+getTrandingMovies();
+addincurfav();
+
+
+// Searchbox Event Listner for each keypress and searching to show suggested results during search on web-page
+searchBox.addEventListener('keyup', () => {
+    let searchString = searchBox.value;
+
+    if (searchString.length > 0) {
+        let searchStringURI = encodeURI(searchString);
+        const searchResult = fetch(`https://api.themoviedb.org/3/search/movie?api_key=cb213741fa9662c69add38c5a59c0110&language=en-US&page=1&include_adult=false&query=${searchStringURI}`)
+            .then((response) => response.json())
+            .then((data) => {
+                currentMovieStack = data.results;
+                renderList("favourite");
+            })
+            .catch((err) => printError(err));
+    }
+})
+
+
+// Function to add movies into favourites 
 function favourite(element) {
     let id = element.dataset.id;
     for (let i = 0; i < currentMovieStack.length; i++) {
-      if (currentMovieStack[i].id == id) {
-        let favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
-  
-        if (favouriteMovies == null) {
-          favouriteMovies = [];
-        }
-  
-        for (let j = 0; j < favouriteMovies.length; j++) {
-          if (favouriteMovies[j].id == id) {
-            showAlert(currentMovieStack[i].title + " Already added to favourite")
-            return;
-          }
-        }
-  
-        favouriteMovies.unshift(currentMovieStack[i]);
-        localStorage.setItem("favouriteMovies", JSON.stringify(favouriteMovies));
-  
-        addincurfav();
-        showAlert(currentMovieStack[i].title + " added to favourite")
-        return;
-      }
-    }
-  }
+        if (currentMovieStack[i].id == id) {
+            let favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
 
-  function addincurfav() {
+            if (favouriteMovies == null) {
+                favouriteMovies = [];
+            }
+
+            for (let j = 0; j < favouriteMovies.length; j++) {
+                if (favouriteMovies[j].id == id) {
+                    showAlert(currentMovieStack[i].title + " Already added to favourite")
+                    return;
+                }
+            }
+
+            favouriteMovies.unshift(currentMovieStack[i]);
+            localStorage.setItem("favouriteMovies", JSON.stringify(favouriteMovies));
+
+            addincurfav();
+            showAlert(currentMovieStack[i].title + " added to favourite")
+            return;
+        }
+    }
+}
+
+function addincurfav() {
     const actionForButton = "remove";
     const favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
-  
+
     if (favouriteMovies !== null) {
-      const ol = document.querySelector(".offcanvas-body > ol");
-      ol.innerHTML = ""
-      favouriteMovies.forEach((movie) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
+        const ol = document.querySelector(".offcanvas-body > ol");
+        ol.innerHTML = ""
+        favouriteMovies.forEach((movie) => {
+            const li = document.createElement("li");
+            li.innerHTML = `
             <img src="${'https://image.tmdb.org/t/p/w500' + movie.poster_path}" alt="${movie.title}">
       <button id="${movie.id}" onclick="getMovieInDetail(this)" class="btn btn-primary"> ${movie.title} </button>
       <button onclick="${actionForButton}(this)" class=" text-icon-button btn btn-danger" data-id="${movie.id}" >
@@ -125,38 +129,38 @@ function favourite(element) {
           <span>${actionForButton}</span>
       </button>
           `;
-        ol.appendChild(li);
-      });
+            ol.appendChild(li);
+        });
     }
-  }
+}
 
-  // remove movies from favourite section
+// Function to Remove movies from favourite section
 function remove(element) {
     let id = element.dataset.id;
     let favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
     let newFavouriteMovies = [];
     for (let i = 0; i < favouriteMovies.length; i++) {
-      if (favouriteMovies[i].id == id) {
-        continue;
-      }
-      newFavouriteMovies.push(favouriteMovies[i]);
+        if (favouriteMovies[i].id == id) {
+            continue;
+        }
+        newFavouriteMovies.push(favouriteMovies[i]);
     }
-  
+
     localStorage.setItem("favouriteMovies", JSON.stringify(newFavouriteMovies));
     currentMovieStack = newFavouriteMovies;
     // renderList("remove");
     addincurfav();
-  }
+}
 
-  // renders movie details on web-page
+// Showing movies details
 function renderMovieInDetail(movie) {
     console.log(movie);
     movieCardContainer.innerHTML = '';
-  
+
     const movieDetailCard = document.createElement('div');
     movieDetailCard.classList.add('detail-movie-card');
     movieDetailCard.style.textAlign = 'center';
-  
+
     movieDetailCard.innerHTML = `
   
   <div class="contai" >
@@ -180,18 +184,17 @@ function renderMovieInDetail(movie) {
               </div>
           </div>
       `;
-  
-    movieCardContainer.append(movieDetailCard);
-  }
 
-  // fetch the defails of of movie and send it to renderMovieDetails to display
+    movieCardContainer.append(movieDetailCard);
+}
+
+// Fetching movie details for sending it to renderMovieDetails to display
 function getMovieInDetail(element) {
 
     fetch(`https://api.themoviedb.org/3/movie/${element.getAttribute('id')}?api_key=cb213741fa9662c69add38c5a59c0110&language=en-US`)
-      .then((response) => response.json())
-      .then((data) => renderMovieInDetail(data))
-      .catch((err) => printError(err));
-  
-  }
-    
+        .then((response) => response.json())
+        .then((data) => renderMovieInDetail(data))
+        .catch((err) => printError(err));
+}
+
   
